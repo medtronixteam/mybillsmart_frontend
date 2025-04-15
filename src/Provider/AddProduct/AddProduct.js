e94c5aab89ccdc6b89982b62aefe6c361c387f72
coder360 <bssit.13.4@gmail.com>
2025-04-15 14:56:33 +0500

    code push

--- src/Provider/AddProduct/AddProduct.js
+++ src/Provider/AddProduct/AddProduct.js
@@ -21,6 +21,7 @@ const AddProduct = () => {
     p4: "",
     p5: "",
     p6: "",
+    points_per_deal:"",
     discount_period_start: "",
     discount_period_end: "",
     meter_rental: "",
@@ -83,144 +84,147 @@ const AddProduct = () => {
   };
 
   return (
-    
-   
-      <div className="add-product-container mx-auto">
-        <h1>Add Products</h1>
-        <form onSubmit={handleSubmit}>
+    <div className="add-product-container mx-auto">
+      <h1>Add Products</h1>
+      <form onSubmit={handleSubmit}>
         <input
-            type="text"
-            name="provider_name"
-            placeholder="Provider Name"
-            value={formData.provider_name}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="product_name"
-            placeholder="Product Name"
-            value={formData.product_name}
-            onChange={handleChange}
-          />
-         
+          type="text"
+          name="provider_name"
+          placeholder="Provider Name"
+          value={formData.provider_name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="product_name"
+          placeholder="Product Name"
+          value={formData.product_name}
+          onChange={handleChange}
+        />
 
-          <input
-            type="text"
-            name="light_category"
-            placeholder="Light Category"
-            value={formData.light_category}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="fixed_rate"
-            placeholder="Fixed Rate"
-            value={formData.fixed_rate}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="rl1"
-            placeholder="RL1"
-            value={formData.rl1}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="rl2"
-            placeholder="RL2"
-            value={formData.rl2}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="rl3"
-            placeholder="RL3"
-            value={formData.rl3}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="p1"
-            placeholder="P1"
-            value={formData.p1}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="p2"
-            placeholder="P2"
-            value={formData.p2}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="p3"
-            placeholder="P3"
-            value={formData.p3}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="p4"
-            placeholder="P4"
-            value={formData.p4}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="p5"
-            placeholder="P5"
-            value={formData.p5}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="p6"
-            placeholder="P6"
-            value={formData.p6}
-            onChange={handleChange}
-          />
-          <div className="date-container">
-            <div className="date-field">
-              <label>Discount Period Start</label>
-              <input
-                type="date"
-                name="discount_period_start"
-                value={formData.discount_period_start}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="date-field">
-              <label>Discount Period End</label>
-              <input
-                type="date"
-                name="discount_period_end"
-                value={formData.discount_period_end}
-                onChange={handleChange}
-              />
-            </div>
+        <input
+          type="text"
+          name="light_category"
+          placeholder="Light Category"
+          value={formData.light_category}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="fixed_rate"
+          placeholder="Fixed Rate"
+          value={formData.fixed_rate}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="rl1"
+          placeholder="RL1"
+          value={formData.rl1}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="rl2"
+          placeholder="RL2"
+          value={formData.rl2}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="rl3"
+          placeholder="RL3"
+          value={formData.rl3}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="p1"
+          placeholder="P1"
+          value={formData.p1}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="p2"
+          placeholder="P2"
+          value={formData.p2}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="p3"
+          placeholder="P3"
+          value={formData.p3}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="p4"
+          placeholder="P4"
+          value={formData.p4}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="p5"
+          placeholder="P5"
+          value={formData.p5}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="p6"
+          placeholder="P6"
+          value={formData.p6}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="points_per_deal"
+          placeholder="points_per_deal"
+          value={formData.points_per_deal}
+          onChange={handleChange}
+        />
+        <div className="date-container">
+          <div className="date-field">
+            <label>Discount Period Start</label>
+            <input
+              type="date"
+              name="discount_period_start"
+              value={formData.discount_period_start}
+              onChange={handleChange}
+            />
           </div>
-          <input
-            type="number"
-            name="meter_rental"
-            placeholder="Meter Rental"
-            value={formData.meter_rental}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="sales_commission"
-            placeholder="Sales Commission"
-            value={formData.sales_commission}
-            onChange={handleChange}
-          />
-        </form>
-        <button type="submit" onClick={handleSubmit}>
-          Add Product
-        </button>
-      </div>
-    
+          <div className="date-field">
+            <label>Discount Period End</label>
+            <input
+              type="date"
+              name="discount_period_end"
+              value={formData.discount_period_end}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <input
+          type="number"
+          name="meter_rental"
+          placeholder="Meter Rental"
+          value={formData.meter_rental}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="sales_commission"
+          placeholder="Sales Commission"
+          value={formData.sales_commission}
+          onChange={handleChange}
+        />
+      </form>
+      <button type="submit" onClick={handleSubmit}>
+        Add Product
+      </button>
+    </div>
   );
 };
 
